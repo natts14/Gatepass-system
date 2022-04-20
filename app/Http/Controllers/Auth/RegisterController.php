12966cb6912d55ce628550ba7bb05715313aacf4
Nathalie Butic <nathalie@crescode.com>
2022-04-21 00:11:58 +0800

    update register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -30,7 +30,7 @@ class RegisterController extends Controller
     public function register(RegisterRequest $request) 
     {
         $user = User::create($request->validated());
-
+        
         auth()->login($user);
 
         return redirect('/login')->with('success', "Account successfully registered.");
