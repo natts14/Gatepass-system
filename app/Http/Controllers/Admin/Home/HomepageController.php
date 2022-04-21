6d961ecc33ca983ac5ddf7df5fff5ddf113627cd
Arjie <arjie.bongcas@splitsecondsoftware.com>
2022-04-21 17:32:13 +0800

    homepage sample

--- app/Http/Controllers/Admin/Home/HomepageController.php
+++ app/Http/Controllers/Admin/Home/HomepageController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Admin\Home;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class HomepageController extends Controller
 {
     public function index() 
     {
-        return view('admin.homepage');
+        $users = User::all();
+        return view('admin.homepage', ['users' => $users]);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -18,12 +18,11 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-
         'status',
         'name',
         'email',
         'password',
-
+        'category'
     ];
 
     public function detail()
